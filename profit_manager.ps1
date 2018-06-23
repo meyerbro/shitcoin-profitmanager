bf07f11dc8c39f4772c139fbcef08f440acca239
randygrolemund <randygrolemund@users.noreply.github.com>
2018-06-23 14:14:02 -0500

    formatting changes

--- profit_manager.ps1
+++ profit_manager.ps1
@@ -1,11 +1,12 @@
 Function Set-WindowSize {
-Param([int]$x=$host.ui.rawui.windowsize.width,
-      [int]$y=$host.ui.rawui.windowsize.heigth)
+    Param([int]$x = $host.ui.rawui.windowsize.width,
+        [int]$y = $host.ui.rawui.windowsize.heigth)
 
-    $size=New-Object System.Management.Automation.Host.Size($x,$y)
-    $host.ui.rawui.WindowSize=$size}
+    $size = New-Object System.Management.Automation.Host.Size($x, $y)
+    $host.ui.rawui.WindowSize = $size
+}
 
-    Set-WindowSize 120 50
+Set-WindowSize 120 50
 
 Write-Host "
                 _______________________________________________________________________________________
@@ -34,7 +35,7 @@ $get_settings = Get-Content -Path "settings.conf" | Out-String | ConvertFrom-Jso
 $default_coin = $get_settings.default_coin
 # How many minutes do you want the miner to run before checking for a new coin?
 $mine_minutes = $get_settings.mining_timer
-$mine_seconds = ($mine_minutes*60)
+$mine_seconds = ($mine_minutes * 60)
 $set_sleep = $get_settings.sleep_seconds
 $enable_voice = $get_settings.voice
 
@@ -51,25 +52,23 @@ $Array = $get_settings.my_coins
 #Check if the best coin to mine is in your list.
 if ($best_coin -in $Array.ToUpper())
 {}
-else
-{
-Write-Host "The best coin to mine is $best_coin but it's not in your list." -ForegroundColor red
-#Choose a default coin to mine if one of the coins listed above is NOT in your list. Prevents the miner from closing when there isn't a match.
-$best_coin = $default_coin
-$bypass_check = "yes"
+else {
+    Write-Host "The best coin to mine is $best_coin but it's not in your list." -ForegroundColor red
+    #Choose a default coin to mine if one of the coins listed above is NOT in your list. Prevents the miner from closing when there isn't a match.
+    $best_coin = $default_coin
+    $bypass_check = "yes"
 }
 
 Write-Host "...Activating Worker on $pc"
 
 #Check folder structure, create missing folders.
-if (Test-Path $path\$pc -PathType Container){
-Write-Host "...Checking Folder Structure. (OK!)" -ForegroundColor green
+if (Test-Path $path\$pc -PathType Container) {
+    Write-Host "...Checking Folder Structure. (OK!)" -ForegroundColor green
 }
-else
-{
-Write-Host "...Creating Folder for $pc" -ForegroundColor yellow
-$fso = new-object -ComObject scripting.filesystemobject
-$fso.CreateFolder("$path\$pc")
+else {
+    Write-Host "...Creating Folder for $pc" -ForegroundColor yellow
+    $fso = new-object -ComObject scripting.filesystemobject
+    $fso.CreateFolder("$path\$pc")
 }
 
 
@@ -92,58 +91,54 @@ Write-Host "   $wallet"
 
 # Verify Diff config file is present
 
-If(Test-Path -Path $Path\$pc\$algo.conf)
-{
-  $set_diff_config = "yes"
-  $set_diff_value = Get-Content -Path "$path\$pc\$algo.conf"
-  write-host "...Diffuculty config for $algo is present, setting to $set_diff_value" -ForegroundColor green
+If (Test-Path -Path $Path\$pc\$algo.conf) {
+    $set_diff_config = "yes"
+    $set_diff_value = Get-Content -Path "$path\$pc\$algo.conf"
+    write-host "...Diffuculty config for $algo is present, setting to $set_diff_value" -ForegroundColor green
   
 }
-else
-{ 
-  write-host "...No diffuculty config for $algo is present. We will skip setting a fixed difficulty until next run." -ForegroundColor red
-  $set_diff_config = "no"
+else { 
+    write-host "...No diffuculty config for $algo is present. We will skip setting a fixed difficulty until next run." -ForegroundColor red
+    $set_diff_config = "no"
 }
 
 # Check for pools.txt file, delete if exists, will create a new one once mining app launches.
-if(Test-Path $path\$pc\pools.txt)
-{
-Write-Host "
+if (Test-Path $path\$pc\pools.txt) {
+    Write-Host "
 ...Purging old Pools.txt file (OK!)"
-del $path\$pc\pools.txt
+    del $path\$pc\pools.txt
 }
-else
-{
-Write-Host "...Could not find Pools.txt file, there is nothing to delete. (OK!)"
+else {
+    Write-Host "...Could not find Pools.txt file, there is nothing to delete. (OK!)"
 }
 
 # These are the default apps used for mining. Updated software can be found at https://github.com/fireice-uk/xmr-stak/releases.
-if ($miner_type -eq 'xmr-stak')
-{Set-Variable -Name "miner_app" -Value "$path\Miner-XMRstak\xmr-stak.exe"
+if ($miner_type -eq 'xmr-stak') {
+    Set-Variable -Name "miner_app" -Value "$path\Miner-XMRstak\xmr-stak.exe"
 }
-if ($miner_type -eq 'arto-stak')
-{Set-Variable -Name "miner_app" -Value "$path\Miner-ArtoMiner\arto-miner.exe"
+if ($miner_type -eq 'arto-stak') {
+    Set-Variable -Name "miner_app" -Value "$path\Miner-ArtoMiner\arto-miner.exe"
 }
 
 Write-Host "...Setting Mining Application to $miner_app"
 
 # This section establishes a fixed diff for each worker. The format depends on which pool you connect to.
-if ($set_diff_config -eq 'yes'){
-if ($diff_config -eq '1')
-{Set-Variable -Name "fixed_diff" -Value "+$set_diff_value"
-}
-if ($diff_config -eq '2')
-{Set-Variable -Name "fixed_diff" -Value ".$set_diff_value"
-}
-if ($diff_config -eq '3')
-{Set-Variable -Name "fixed_diff" -Value ".$pc+$set_diff_value"
-}
-if ($diff_config -eq '4')
-{Set-Variable -Name "fixed_diff" -Value ".$pc"
-}
+if ($set_diff_config -eq 'yes') {
+    if ($diff_config -eq '1') {
+        Set-Variable -Name "fixed_diff" -Value "+$set_diff_value"
+    }
+    if ($diff_config -eq '2') {
+        Set-Variable -Name "fixed_diff" -Value ".$set_diff_value"
+    }
+    if ($diff_config -eq '3') {
+        Set-Variable -Name "fixed_diff" -Value ".$pc+$set_diff_value"
+    }
+    if ($diff_config -eq '4') {
+        Set-Variable -Name "fixed_diff" -Value ".$pc"
+    }
 }
 else {
- Set-Variable -Name "fixed_diff" -Value ""
+    Set-Variable -Name "fixed_diff" -Value ""
 }
 
 # Configure the attributes for the mining software.
@@ -170,59 +165,59 @@ write-host "Check Profitiability: $TimeEnd
 " -ForegroundColor Green
 
 # If we are mining the default coin, pause for 5 minutes.
-if ($bypass_check -eq 'yes'){
- Write-Host $TimeNow : "Currently mining default coin: $best_coin : Checking again at $TimeEnd" -ForegroundColor White
-Start-Sleep -Seconds $mine_seconds
+if ($bypass_check -eq 'yes') {
+    Write-Host $TimeNow : "Currently mining default coin: $best_coin : Checking again at $TimeEnd" -ForegroundColor White
+    Start-Sleep -Seconds $mine_seconds
 }
 
 # Begin a loop to check if the current coin is the best coin to mine. If not, restart the app and switchin coins.
 Do { 
- $TimeNow = Get-Date
-  if ($TimeNow -ge $TimeEnd) {
-  $get_coin_check = Invoke-RestMethod -Uri "https://minecryptonight.net/api/best" -Method Get
-  $best_coin_check = $get_coin_check.current
+    $TimeNow = Get-Date
+    if ($TimeNow -ge $TimeEnd) {
+        $get_coin_check = Invoke-RestMethod -Uri "https://minecryptonight.net/api/best" -Method Get
+        $best_coin_check = $get_coin_check.current
   
-  Write-host $TimeNow : "Checking Coin Profitability."
-  Write-Host $TimeNow : "Best Coin to Mine:" $best_coin_check -ForegroundColor Yellow
+        Write-host $TimeNow : "Checking Coin Profitability."
+        Write-Host $TimeNow : "Best Coin to Mine:" $best_coin_check -ForegroundColor Yellow
   
-  if ($best_coin -eq $best_coin_check) {
+        if ($best_coin -eq $best_coin_check) {
   
-  Write-Host $TimeNow : "Sleeping for another" $set_sleep "seconds, then checking again."
-  }
- } else {
+            Write-Host $TimeNow : "Sleeping for another" $set_sleep "seconds, then checking again."
+        }
+    }
+    else {
   
-  Write-Host $TimeNow : "Currently mining $best_coin : Checking again at $TimeEnd."
- }
-  # Get the current hashrate from mining software
-  $get_hashrate = Invoke-RestMethod -Uri "http://127.0.0.1:8080/api.json" -Method Get 
-  $worker_hashrate = $get_hashrate.hashrate.total[0]
-  $my_results = $get_hashrate.results.shares_good
-  $suggested_diff = [math]::Round($worker_hashrate*30)
-  if ($worker_hashrate -match "[0-9]") {
-    Write-Host $TimeNow : "Worker Hashrate:" $worker_hashrate "H/s, Accepted Shares: $my_results" -ForegroundColor Green
+        Write-Host $TimeNow : "Currently mining $best_coin : Checking again at $TimeEnd."
+    }
+    # Get the current hashrate from mining software
+    $get_hashrate = Invoke-RestMethod -Uri "http://127.0.0.1:8080/api.json" -Method Get 
+    $worker_hashrate = $get_hashrate.hashrate.total[0]
+    $my_results = $get_hashrate.results.shares_good
+    $suggested_diff = [math]::Round($worker_hashrate * 30)
+    if ($worker_hashrate -match "[0-9]") {
+        Write-Host $TimeNow : "Worker Hashrate:" $worker_hashrate "H/s, Accepted Shares: $my_results" -ForegroundColor Green
     
-} else {
+    }
+    else {
         Write-Host $TimeNow : "Waiting on worker to warm up before displaying hashrate." -ForegroundColor Cyan
-}
+    }
   
-  Start-Sleep -Seconds $set_sleep
+    Start-Sleep -Seconds $set_sleep
 }
 While ($best_coin -eq $best_coin_check)
 
-  if ($enable_voice -eq 'yes'){
-# Speak the symbol of the coin when switching.
-$speak_coin = ("$best_coin_check" -split "([a-z0-9]{1})"  | ?{ $_.length -ne 0 }) -join " "
-Add-Type -AssemblyName System.Speech
-$synthesizer = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
-$synthesizer.Speak("$pc is switching to $speak_coin") | Out-Null
+if ($enable_voice -eq 'yes') {
+    # Speak the symbol of the coin when switching.
+    $speak_coin = ("$best_coin_check" -split "([a-z0-9]{1})"  | ? { $_.length -ne 0 }) -join " "
+    Add-Type -AssemblyName System.Speech
+    $synthesizer = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+    $synthesizer.Speak("$pc is switching to $speak_coin") | Out-Null
 }
-If( Test-Path -Path $Path\$pc\$algo.conf )
-{
+If ( Test-Path -Path $Path\$pc\$algo.conf ) {
     write-host "Diffuculty config for $algo is present, no need to create a new config."
 
 }
-else
-{
+else {
     Write-Host "Creating difficulty config file for $algo on this worker. We've calulated the fixed difficulty to be $suggested_diff" -ForegroundColor Green
     $suggested_diff | Out-File $path\$pc\$algo.conf
 }
