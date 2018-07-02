10b8405d3450a0d2462e72b49a624672661fec3c
randygrolemund <randygrolemund@users.noreply.github.com>
2018-07-02 10:15:34 -0500

    Update bittube bug

--- profit_manager.ps1
+++ profit_manager.ps1
@@ -130,7 +130,7 @@ if ($miner_type -eq 'xmr-stak') {
 if ($miner_type -eq 'arto-stak') {
     Set-Variable -Name "miner_app" -Value "$path\Miner-ArtoMiner\arto-miner.exe"
 }
-if ($miner_type -eq 'bittube-miner.exe') {
+if ($miner_type -eq 'bittube-miner') {
     Set-Variable -Name "miner_app" -Value "$path\Miner-Bittube\bittube-miner.exe"
 }
 
